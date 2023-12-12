c2b09c4342adfbcdb91de435e13d4546fb61537a
Andy4433 <anderosn.s.brito613@gmail.com>
2023-12-12 20:59:01 -0300

    feat: front-end pagina contato (editar,cria,remover e index) criaçao do ContatoController e views cotato

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App2.Controllers
+{
+
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult Criar()
+        {
+            return View();
+        }
+         public IActionResult Deletar()
+        {
+            return View();
+        }
+         public IActionResult Editar()
+        {
+            return View();
+        }
+
+    }
+}
